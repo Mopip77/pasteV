06b30e3e834c33ea55b4e80197244be4c6b9b2d9
mopip77 <mopip77@qq.com>
2024-08-12 15:05:38 +0800

    feat: listen escape to hide app

--- renderer/pages/_app.tsx
+++ renderer/pages/_app.tsx
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import type { AppProps } from "next/app";
 
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    const handleEsc = (e: KeyboardEvent) => {
+      if (e.key === "Escape") {
+        window.ipc.send("app:hide", undefined);
+      }
+    };
+
+    window.addEventListener("keydown", handleEsc);
+
+    return () => {
+      window.removeEventListener("keydown", handleEsc);
+    };
+  }, []);
+
   return <Component {...pageProps} />;
 }
 
